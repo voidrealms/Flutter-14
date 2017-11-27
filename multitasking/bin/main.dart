fc5c23db9065457066ab3fa4d8b7d9dee107cf28
Rootshell <bcairns@voidrealms.com>
2017-11-27 13:47:04 -0500

    First Commit

---
+++ multitasking/bin/main.dart
@@ -0,0 +1,60 @@
+import 'dart:async';
+
+Future<bool> longWait(String prefix) async {
+  for(int i = 0 ; i < 5; i ++) {
+    print('$prefix $i');
+  }
+  return true;
+}
+
+Future testAsync() async {
+  print('starting');
+  bool ret = await longWait('Async');
+  print(ret);
+  print('done');
+}
+
+void testThen() {
+  print('starting');
+
+  longWait('Then').then((bool value) {
+    print('done waiting');
+  });
+
+  print('done');
+}
+
+void testChain() {
+  print('starting');
+
+  Future f = longWait('Chains');
+  f.then((bool value) {
+    print('done 1');
+  }).then((bool value) {
+    print('done 2');
+  }).then((bool value) {
+    print('done 3');
+  });
+
+  print('done');
+}
+
+Future testMultiple() async {
+  print('starting');
+
+  Future f1 = longWait('one');
+  Future f2 = longWait('two');
+  Future f3 = longWait('three');
+
+  await Future.wait([f1,f2,f3]);
+
+  print('done');
+}
+
+main(List<String> arguments) {
+  //testAsync();
+  //testThen();
+  //testChain();
+  testMultiple();
+
+}
---
+++ readme.txt
@@ -0,0 +1,11 @@
+
+Code for video tutorial
+
+Title: Flutter 14 - Async programming
+Published: 11/27/2017 1:45 PM
+Link: https://youtu.be/MfLapF8d5gg
+
+Description: Learn how to work with async programming
+
+If you found this helpful, please consider donating: http://www.voidrealms.com
+
